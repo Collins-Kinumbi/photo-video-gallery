0a0990613b333fee9e04eacb03a0a69626c98f32
Collins Kinumbi <kinumbicollins254@gmail.com>
2024-06-18 13:35:44 +0300

    About page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,42 @@
 function page() {
   return (
     <main>
-      <div className="content">about</div>
+      <div className="content">
+        <div className="about">
+          <p>
+            This photo gallery / video site is my fun portfolio project I did
+            using the{" "}
+            <a
+              href="https://www.pexels.com/api"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Pexels API
+            </a>{" "}
+            to fetch picture and video data and used the Next js framework to
+            build it. You can search for photos and videos and also download
+            photos in their original quality.
+          </p>
+          <p>
+            My GitHub is{" "}
+            <a
+              href="https://github.com/Collins-Kinumbi"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Collins Kinumbi
+            </a>{" "}
+            and my Twitter is{" "}
+            <a
+              href="https://x.com/collinsKinumbi"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              @collinsKinumbi
+            </a>
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
